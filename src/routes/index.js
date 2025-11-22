a2c4ff062a1841da433472cef077c7784d4b64c8
JorgeEscalonas <escalonajorge73@gmail.com>
2025-11-21 20:59:53 -0400

    feat(crud-pagos): Implementación de CRUD y lógica de pagos, con actualización del estado de Préstamo.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import authRouter from "./auth.routes.js";
 import clienteRouter from "./cliente.routes.js";
 import tasaRouter from "./tasa.routes.js";
 import prestamoRouter from "./prestamo.routes.js";
+import paymentRoutes from "./pagos.routes.js";
 
 const router = Router();
 
@@ -15,5 +16,6 @@ router.get("/", (req, res) => {
 router.use("/clientes", clienteRouter);
 router.use("/tasas", tasaRouter);
 router.use("/prestamos", prestamoRouter);
+router.use("/pagos", paymentRoutes);
 
 export default router;
